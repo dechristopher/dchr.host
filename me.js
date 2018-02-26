64458c41e4cfe222c115edd33c2e9fb71bfdedbc
Andrew DeChristopher <andrewdechristopher@gmail.com>
2018-02-25 19:28:04 -0500

    Implemented me.js script

---
+++ me.js
@@ -0,0 +1,9 @@
+console.log(`© 2018 Andrew DeChristopher ; contact for inquiries or job opportunities`);
+
+setTimeout(function () {
+	document.getElementById('me').innerText = document.getElementById('me').innerText.replace('${me}', 'dechristopher').replace('${me}', 'dechristopher').replace('${me}', 'dechristopher');
+}, 25000);
+
+setTimeout(function () {
+	document.getElementById('me').innerText = document.getElementById('me').innerText + '\nwhy are you still here?';
+}, 35000);
