190f4c9ab0280df80b6e89937101d1734368cdf4
Andrew DeChristopher <andrewdechristopher@gmail.com>
2018-02-26 11:01:04 -0500

    Renamed index.html to index.php

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
+<?php
+$use_sts = true;
+
+// Force HTTPS (Strict Transport Security)
+if ($use_sts && isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') {
+	header('Strict-Transport-Security: max-age=31536000');
+} elseif ($use_sts) {
+	header('Location: https://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'], true, 301);
+	die();
+}
+?>
 <!DOCTYPE html>
 <html lang="en" aria-label="dchr.host">
 <head>
