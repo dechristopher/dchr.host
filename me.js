db7ae1d3350aebf095fa86f24accbd9783084e72
Andrew DeChristopher <andrewdechristopher@gmail.com>
2018-02-26 22:25:00 -0500

    Removed accidental # in id reference

--- me.js
+++ me.js
@@ -3,7 +3,7 @@ console.log(`© 2018 Andrew DeChristopher ; contact for inquiries or job opportu
 $('.to').textillate({ in: { effect: 'fadeInDown', shuffle: true } });
 
 setTimeout(function () {
-	document.getElementById('#me').innerHTML = document.getElementById('#me').innerHTML + '\n<i class=".y">why are you still here?</i>';
+	document.getElementById('me').innerHTML = document.getElementById('me').innerHTML + '\n<i class="y">why are you still here?</i>';
 	$('.y').textillate({ in: { effect: 'fadeInUp' } });
 }, 3000);
 
