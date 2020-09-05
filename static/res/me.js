4ff811cae709b1a6a965630b20018f5d2f360cfb
Andrew DeChristopher <andrewdechristopher@gmail.com>
2020-09-05 11:57:52 -0400

    Removed unnecessary script file

--- static/res/me.js
+++
@@ -1,6 +0,0 @@
-console.log(`© 2020 Andrew DeChristopher ; contact for inquiries or job opportunities`);
-
-$(document).ready(()  => {
-	$('.to').textillate({ in: { effect: 'fadeInDown', shuffle: true } });
-});
-
